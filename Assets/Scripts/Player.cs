1953929f1fbecb37797f6a8ca15c82395b2d7639
06-AltonKoh <jojiahao@gmail.com>
2021-10-19 20:39:14 +0800

    Code Finish

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public GameObject playerob;
     private Animation thisAnimation;
+
+    public int PointCount;
+    public Text Pointtxt;
 
     void Start()
     {
@@ -14,11 +20,33 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(playerob.transform.position.y <= 3.5 && playerob.transform.position.y >= -3.5)
         {
-            thisAnimation.Play();
-            GetComponent<Rigidbody>().AddForce(0, 3.5f, 0, ForceMode.Impulse);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                thisAnimation.Play();
+                GetComponent<Rigidbody>().AddForce(0, 3.5f, 0, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene("GameOverScene");
         }
             
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Point")
+        {
+            PointCount += 1;
+            Pointtxt.text = "SCORE: " + PointCount;
+        }
     }
 }
